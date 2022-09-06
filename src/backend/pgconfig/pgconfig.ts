f8953ef54853e31e8e2dd540785902442b6f4fae
Cesar Alaestante <cesara@gmail.com>
2022-09-05 18:09:41 -0700

    chore: remove supabase, remove start command from package.json

--- src/backend/pgconfig/pgconfig.ts
+++ src/backend/pgconfig/pgconfig.ts
@@ -3,10 +3,7 @@ import type { Executor } from "../pg.js";
 import { PGMemConfig } from "./pgmem.js";
 import { PostgresDBConfig } from "./postgres.js";
 
-/**
- * We use Postgres in a few different ways: directly, via supabase,
- * emulated with pg-mem. This interface abstracts their differences.
- */
+
 export interface PGConfig {
   initPool(): Pool;
   getSchemaVersion(executor: Executor): Promise<number>;
